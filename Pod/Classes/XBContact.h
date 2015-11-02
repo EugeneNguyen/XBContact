68d91c48f03f91af32c448ba713c02b82b385303
Binh Nguyen Xuan <binhnguyenxuan@Binhs-MacBook-Pro.local>
2015-11-02 11:42:53 +0700

    First commit

---
+++ Pod/Classes/XBContact.h
@@ -0,0 +1,13 @@
+//
+//  XBContact.h
+//  Pods
+//
+//  Created by Binh Nguyen Xuan on 11/2/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XBContact : NSObject
+
+@end
---
+++ Pod/Classes/XBContact.m
@@ -0,0 +1,13 @@
+//
+//  XBContact.m
+//  Pods
+//
+//  Created by Binh Nguyen Xuan on 11/2/15.
+//
+//
+
+#import "XBContact.h"
+
+@implementation XBContact
+
+@end
